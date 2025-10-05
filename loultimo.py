19769cbe39abc6a2c0965dcf58c8a3679a613f56
po11byte <po1187042022@unab.edu.sv>
2025-10-05 15:30:47 -0600

    Update loultimo.py

--- loultimo.py
+++ loultimo.py
@@ -2,15 +2,25 @@ import streamlit as st
 import requests
 
 st.title("Situación climática")
-st.write("Escriba el departamento: ")
-departamento = st.text_input("Escriba el departamento")
+st.write("Seleccione el departamento: ")
+
+
+departamentos_el_salvador = [
+    "Ahuachapán", "Cabañas", "Chalatenango", "Cuscatlán", "La Libertad",
+    "La Paz", "La Unión", "Morazán", "San Miguel", "San Salvador",
+    "San Vicente", "Santa Ana", "Sonsonate", "Usulután"
+]
 
 API_KEY = "1aefb8a907db6f0953a604ab4d387020"
 
 info = []
 
+
+departamento = st.selectbox("Seleccione el departamento", options=[""] + departamentos_el_salvador)
+
 with st.sidebar:
-    departamento2 = st.text_input("Escriba el departamento")
+    
+    departamento2 = st.selectbox("Seleccione el departamento", options=[""] + departamentos_el_salvador)
     st.button("ver clima")
 
 if st.button("Ver clima"):
@@ -22,22 +32,22 @@ if st.button("Ver clima"):
         data = response.json()
         
         if response.status_code == 200:
-           # st.success(data)
+           
             info = {
                 "country" : data["sys"]["country"],
                 "city": data["name"],
-                "temp" : f"{data["main"]["temp"]}°C",
-                "humidity" : f"{data["main"]["humidity"]}%",
+                "temp" : f"{data['main']['temp']}°C",
+                "humidity" : f"{data['main']['humidity']}%",
                 "description" : data["weather"][0]["description"],
-                "wind" : f"{data["wind"]["speed"]} km/h"
+                "wind" : f"{data['wind']['speed']} km/h"
             }
-            st.success(f"{info["city"]}")
-            st.success(f"{info["temp"]}")
-            st.success(f"{info["humidity"]}")
-            st.success(f"{info["description"]}")
-            st.success(f"{info["wind"]}")
+            st.success(f"{info['city']}")
+            st.success(f"{info['temp']}")
+            st.success(f"{info['humidity']}")
+            st.success(f"{info['description']}")
+            st.success(f"{info['wind']}")
             
         else:
             st.error("Error al obtener los datos del clima")
     else:
-        st.warning("Por favor, ingrese un departamento")
+        st.warning("Por favor, seleccione un departamento")
