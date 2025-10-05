2773885d5f8dfe189d4d2114f3ba553c2e7de439
po11byte <po1187042022@unab.edu.sv>
2025-10-05 15:40:20 -0600

    Update loultimo.py

--- loultimo.py
+++ loultimo.py
@@ -16,12 +16,13 @@ API_KEY = "1aefb8a907db6f0953a604ab4d387020"
 info = []
 
 
-departamento = st.selectbox("Seleccione el departamento", options=[""] + departamentos_el_salvador)
+departamento = st.selectbox("Seleccione el departamento", options=departamentos_el_salvador)
 
 with st.sidebar:
     
-    departamento2 = st.selectbox("Seleccione el departamento", options=[""] + departamentos_el_salvador)
-    st.button("ver clima")
+    departamento2 = st.selectbox("Seleccione el departamento", options=departamentos_el_salvador, key="sidebar_select")
+    ver_clima_sidebar = st.button("ver clima")
+
 
 if st.button("Ver clima"):
     if departamento:
@@ -33,6 +34,35 @@ if st.button("Ver clima"):
         
         if response.status_code == 200:
            
+            info = {
+                "country" : data["sys"]["country"],
+                "city": data["name"],
+                "temp" : f"{data['main']['temp']}°C",
+                "humidity" : f"{data['main']['humidity']}%",
+                "description" : data["weather"][0]["description"],
+                "wind" : f"{data['wind']['speed']} km/h"
+            }
+            st.success(f"{info['city']}")
+            st.success(f"{info['temp']}")
+            st.success(f"{info['humidity']}")
+            st.success(f"{info['description']}")
+            st.success(f"{info['wind']}")
+            
+        else:
+            st.error("Error al obtener los datos del clima")
+    else:
+        st.warning("Por favor, seleccione un departamento")
+
+
+if ver_clima_sidebar:
+    if departamento2:
+        
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={departamento2}&appid={API_KEY}&units=metric"
+        
+        response = requests.get(url)
+        data = response.json()
+        
+        if response.status_code == 200:
             info = {
                 "country" : data["sys"]["country"],
                 "city": data["name"],
